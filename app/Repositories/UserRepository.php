04e67f52433f930c416de7487fc1143f78771a6b
vy-phan <phanluvy14102005@gmail.com>
2025-05-15 21:04:52 +0700

    first commit ne

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use App\Repositories\Interfaces\UserRepositoryInterface;
+
+class UserRepository implements UserRepositoryInterface
+{
+    public function getAllUsers()
+    {
+        return User::all();
+    }
+
+    public function getUserById($id)
+    {
+        return User::find($id);
+    }
+
+    public function createUser(array $data)
+    {
+        return User::create($data);
+    }
+
+    public function updateUser($id, array $data)
+    {
+        $user = User::find($id);
+        if ($user) {
+            $user->update($data);
+            return $user;
+        }
+        return null;
+    }
+
+    public function deleteUser($id)
+    {
+        $user = User::find($id);
+        if ($user) {
+            return $user->delete();
+        }
+        return false;
+    }
+}
